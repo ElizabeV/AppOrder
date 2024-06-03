93939e1730fad6806d991ed47f8fdad9627b2d13
Elizabeth Viricheva <liza.vir@ya.ru>
2024-06-03 22:08:12 +0300

    nothing to work

--- src/test/java/ru/netology/SendFormTest.java
+++ src/test/java/ru/netology/SendFormTest.java
@@ -1,21 +1,61 @@
 package ru.netology;
 
-import com.codeborne.selenide.SelenideElement;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class SendFormTest {
+
+class SendFormTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    public static void setupAll() {
+      WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    public void BeforeEach() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999");
+    }
+
+    @AfterEach
+    public void AfterEach() {
+        driver.quit();
+        driver = null;
+    }
+
     @Test
     void shouldFillCheckAndClick() {
-        open("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("ЭЭЭ ЭЭ");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+78005553535");
+        driver.findElement(By.cssSelector("[data-test-id='agreement'] input")).click();
+        driver.findElement(By.cssSelector("button.button")).click();
+        var actualTextElement = driver.findElement(By.cssSelector("[data-test-id=order-success]"));
+        var actualText = actualTextElement.getText().trim();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actualText);
+        assertTrue(actualTextElement.isDisplayed());
+        /* open("http://localhost:9999");
         //SelenideElement form = $("[data-test-id]=name);
         $("[data-test-id='name'] input").setValue("Римский-Корсаков Николай");
         $("[data-test-id='phone'] input").setValue("+79019019012");
         $("[data-test-id='agreement']").click();
         $("[button.button]").click();
+        $("[data-test-id='order-success']").shouldHave(exactText("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }*/
 
     }
-
 }
